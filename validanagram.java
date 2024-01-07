93c423707f8c97d741c89af43cc2d51c86b88d67
thinhdng <thinh.d.nguyen1341@gmail.com>
2024-01-06 23:10:20 -0600

    neetcode day 2 time complexity added

--- validanagram.java
+++ validanagram.java
@@ -1,21 +1,12 @@
-import java.util.Arrays;
-public class validanagram {
-
-    public static void main(String[] args){
-       String s = "anagram", t = "nagaram";
-
-       isAnagram(s, t);
-    }
-
-    public static void isAnagram(String s, String t) {
+class Solution {
+    public boolean isAnagram(String s, String t) {
         String[] sArray = s.split("", 0);
         String[] tArray = t.split("", 0);
-
         Arrays.sort(sArray);
         Arrays.sort(tArray);
 
-        System.out.println(Arrays.toString(sArray));
-        System.out.println(Arrays.toString(tArray));
-        System.out.println(Arrays.equals(sArray, tArray));
+        return Arrays.equals(sArray, tArray);
+
+        //time complexity: O(nlgn) => sorting takes O(nlgn) time
     }
 }
