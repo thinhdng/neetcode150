e8eead16e2684a7b2c983dfff556d903e42460b8
thinhdng <thinh.d.nguyen1341@gmail.com>
2024-01-08 10:29:41 -0600

    neetcode day 1 added solution using hashmap

--- containsduplicate.java
+++ containsduplicate.java
@@ -10,3 +10,20 @@ class Solution {
         //time complexity: O(n + nlgn) => sorting takes nlgn time
     }
 }
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Integer> numsMap = new HashMap<Integer, Integer>();
+
+        for(int i = 0; i < nums.length; i++){
+            if(numsMap.containsKey(nums[i])){
+                return true;
+            }
+            numsMap.put(nums[i], i);
+        }
+
+        return false;
+    }
+    //time complexity: O(n) worst case using hashmap, since it only iterates through the array only once
+    //and the put and contains key methods of a hashmap is only O(1)
+}
--- containsduplicate.py
+++ containsduplicate.py
@@ -6,3 +6,15 @@ class Solution:
                 return True
 
         return False 
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        numsMap = {}
+
+        for i in range(0, len(nums)):
+            if(nums[i] in numsMap):
+                return True
+            
+            numsMap[nums[i]] = i
+
+        return False
